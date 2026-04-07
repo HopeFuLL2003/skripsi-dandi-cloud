e257267b9dca5f1a77f510df96c8b6f63d2a9611
HopeFuLL2003 <dandikusuma03@gmail.com>
2026-04-07 13:30:44 +0700

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+import face_recognition
+import numpy as np
+import cv2
+import pickle
 from flask import Flask, request, jsonify
 from datetime import datetime
+
+ENCODING_FILE = "encodings.pkl"
+
+with open(ENCODING_FILE, "rb") as f:
+    data = pickle.load(f)
+
+known_encodings = data["encodings"]
+known_names = data["names"]
 
 app = Flask(__name__)
 
@@ -71,3 +83,45 @@ def log_access():
 @app.route("/logs")
 def get_logs():
     return jsonify(access_logs)
+
+@app.route("/check_face", methods=["POST"])
+def check_face():
+
+    img_bytes = request.data
+
+    if not img_bytes or len(img_bytes) < 100:
+        return jsonify({"result": "NO_IMAGE", "name": "Unknown"})
+
+    np_img = np.frombuffer(img_bytes, np.uint8)
+    image = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+
+    if image is None:
+        return jsonify({"result": "ERROR", "name": "Unknown"})
+
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    boxes = face_recognition.face_locations(rgb, model="hog")
+    encodings = face_recognition.face_encodings(rgb, boxes)
+
+    if len(encodings) == 0:
+        return jsonify({"result": "NO_FACE", "name": "Unknown"})
+
+    encoding = encodings[0]
+
+    distances = face_recognition.face_distance(known_encodings, encoding)
+    best_match_index = np.argmin(distances)
+
+    if distances[best_match_index] < 0.5:
+
+        name = known_names[best_match_index]
+
+        return jsonify({
+            "result": "FACE_OK",
+            "name": name
+        })
+
+    else:
+        return jsonify({
+            "result": "UNKNOWN",
+            "name": "Unknown"
+        })
